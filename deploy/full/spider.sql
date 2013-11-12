f594802cbc6584529c89d8382834869169a7154b
rosicky1985 <rosicky1985@163.com>
2013-11-12 08:40:47 +0800

    use spider_junior_test as default test db

--- deploy/full/spider.sql
+++ deploy/full/spider.sql
@@ -1,70 +1,251 @@
-create database if not exists `spider_junior` default character set 'utf8';
-use `spider_junior`;
-create table if not exists `item`(
-	`id` int(11) NOT NULL auto_increment,
-	`title` varchar(64) NOT NULL,
-	`rank` int(11) NOT NULL,
-	`index` int(11) NOT NULL,
-	`director_id` int(11) NULL,
-	`data_source_id` int(11) NOT NULL,
-	`area` varchar(64) NULL,
-	`task_id` int(11) NOT NULL,
-	primary key(`id`)
-)ENGINE=InnodDb DEFAULT CHARSET=utf8;
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: spider_junior
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.12.04.1
 
-create table if not exists `item_actors`(
-	`id` int(11) NOT NULL auto_increment,
-	`item_id` int(11) NOT NULL,
-	`actor_id` int(11) NOT NULL,
-	primary key(`id`)
-)ENGINE=InnodDb DEFAULT CHARSET=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table if not exists `item_categories`(
-	`id` int(11) NOT NULL auto_increment,
-	`item_id` int(11) NOT NULL,
-	`category_id` int(11) NOT NULL,
-	primary key(`id`)
-)ENGINE=InnodDb DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `category`
+--
 
-create table if not exists `person`(
-	`id` int(11) NOT NULL auto_increment,
-	`name` varchar(64) NOT NULL,
-	primary key(`id`)
-)ENGINE=InnodDb DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table if not exists `category`(
-	`id` int(11) NOT NULL auto_increment,
-	`name` varchar(64) NOT NULL,
-	primary key(`id`)
-)ENGINE=InnodDb DEFAULT CHARSET=utf8;
+--
+-- Dumping data for table `category`
+--
 
-create table if not exists `company`(
-	`id` int(11) NOT NULL auto_increment,
-	`name` varchar(64) NOT NULL,
-	primary key(`id`)
-)ENGINE=InnodDb DEFAULT CHARSET=utf8;
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
-create table if not exists `type`(
-	`id` int(11) NOT NULL auto_increment,
-	`name` varchar(64) NOT NULL,
-	primary key(`id`)
-)ENGINE=InnodDb DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `company`
+--
 
-create table if not exists `data_source`(
-	`id` int(11) NOT NULL auto_increment,
-	`name` varchar(64) NOT NULL,
-	`company_id` int(11) NOT NULL,
-	`url` varchar(256) NOT NULL,
-	`type_id` int(11) NOT NULL,
-	primary key(`id`)
-)ENGINE=InnodDb DEFAULT CHARSET=utf8;
-ALTER TABLE `data_source` ADD COLUMN `bean` VARCHAR(64) NULL  AFTER `type_id` , ADD COLUMN `data_sourcecol` VARCHAR(45) NOT NULL  AFTER `bean` ;
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table if not exists `task`(
-	`id` int(11) NOT NULL auto_increment,
-	`created` datetime NOT NULL ,
-	`target` datetime NOT NULL ,
-	primary key(`id`)
-)ENGINE=InnoDb DEFAULT CHARSET=utf8;
+--
+-- Dumping data for table `company`
+--
 
-GRANT ALL PRIVILEGES ON `spider_junior`.* TO 'rosicky'@'localhost' identified by 'love';
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+INSERT INTO `company` VALUES (1,'优酷'),(2,'迅雷'),(3,'百度'),(4,'奇艺'),(5,'搜狐'),(6,'乐视');
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `data_source`
+--
+
+DROP TABLE IF EXISTS `data_source`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `data_source` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `url` varchar(256) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `bean` varchar(64) NOT NULL,
+  `contenttype` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data_source`
+--
+
+LOCK TABLES `data_source` WRITE;
+/*!40000 ALTER TABLE `data_source` DISABLE KEYS */;
+INSERT INTO `data_source` VALUES (1,'百度电视剧日报',3,'http://top.baidu.com/buzz.php?p=tv',1,'baiduDailyTaskRunner','电视剧'),(2,'百度电影日报',3,'http://top.baidu.com/buzz.php?p=movie',1,'baiduDailyTaskRunner','电影'),(3,'看看电视剧日报',2,'http://movie.kankan.com/top/teleplay.html',1,'kanKanDailyTaskRunner','电视剧'),(4,'看看电影日报',2,'http://movie.kankan.com/top/movie.html',1,'kanKanDailyTaskRunner','电影'),(5,'乐视电视剧日报',6,'http://top.letv.com/tvhp.html',1,'letvDailyTaskRunner','电视剧'),(6,'乐视电视剧周报',6,'http://top.letv.com/tvhp.html',2,'letvWeeklyTaskRunner','电视剧'),(7,'乐视电视据月报',6,'http://top.letv.com/tvhp.html',3,'letvMonthlyTaskRunner','电视剧'),(8,'乐视电影日报',6,'http://top.letv.com/filmhp.html',1,'letvDailyTaskRunner','电影'),(9,'乐视电影周报',6,'http://top.letv.com/filmhp.html',2,'letvWeeklyTaskRunner','电影'),(10,'乐视电影月报',6,'http://top.letv.com/filmhp.html',3,'letvMonthlyTaskRunner','电影'),(11,'奇艺电视剧日报',4,'http://top.inter.qiyi.com/index/front?cid=2&dim=day&len=50&area=top&time=1380693814851',1,'qiyiDailyTaskRunner','电视剧'),(12,'奇艺电视剧周报',4,'http://top.inter.qiyi.com/index/front?cid=2&dim=wee&len=50&area=top&time=1380693814851',2,'qiyiWeeklyTaskRunner','电视剧'),(13,'奇艺电影日报',4,'http://top.inter.qiyi.com/index/front?cid=1&dim=day&len=50&area=top&time=1380693546402',1,'qiyiDailyTaskRunner','电影'),(14,'奇艺电影周报',4,'http://top.inter.qiyi.com/index/front?cid=1&dim=wee&len=50&area=top&time=1380693546402',2,'qiyiWeeklyTaskRunner','电影'),(15,'搜狐电视剧日报',5,'http://tv.sohu.com/frag/vrs_inc/phb_tv_day_50.js',1,'sohuTaskRunner','电视剧'),(16,'搜狐电视剧周报',5,'http://tv.sohu.com/frag/vrs_inc/phb_tv_week_50.js',2,'sohuTaskRunner','电视剧'),(17,'搜狐电视剧月报',5,'http://tv.sohu.com/frag/vrs_inc/phb_tv_month_50.js',3,'sohuTaskRunner','电视剧'),(18,'搜狐电影日报',5,'http://tv.sohu.com/frag/vrs_inc/phb_mv_day_50.js',1,'sohuTaskRunner','电影'),(19,'搜狐电影周报',5,'http://tv.sohu.com/frag/vrs_inc/phb_mv_week_50.js',2,'sohuTaskRunner','电影'),(20,'搜狐电影月报',5,'http://tv.sohu.com/frag/vrs_inc/phb_mv_month_50.js',3,'sohuTaskRunner','电影'),(21,'优酷电视剧日报',1,'http://index.youku.com/protop/0',1,'youkuDailyTaskRunner','电视剧'),(22,'优酷电影日报',1,'http://index.youku.com/protop/2',1,'youkuDailyTaskRunner','电影');
+/*!40000 ALTER TABLE `data_source` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `rank` int(11) NOT NULL,
+  `index` int(11) NOT NULL,
+  `director_id` int(11) DEFAULT NULL,
+  `data_source_id` int(11) NOT NULL,
+  `area` varchar(64) DEFAULT NULL,
+  `task_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_actors`
+--
+
+DROP TABLE IF EXISTS `item_actors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item_actors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL,
+  `actor_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_actors`
+--
+
+LOCK TABLES `item_actors` WRITE;
+/*!40000 ALTER TABLE `item_actors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_actors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_categories`
+--
+
+DROP TABLE IF EXISTS `item_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_categories`
+--
+
+LOCK TABLES `item_categories` WRITE;
+/*!40000 ALTER TABLE `item_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `person`
+--
+
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `person`
+--
+
+LOCK TABLES `person` WRITE;
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
+  `target` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `type`
+--
+
+DROP TABLE IF EXISTS `type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `type`
+--
+
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+INSERT INTO `type` VALUES (1,'日报'),(2,'周报'),(3,'月报');
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-10-08 11:09:04
